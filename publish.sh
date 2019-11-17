811a7ee67828c4d7283a7f8bde594590756beecc
Liyulun <466214245@qq.com>
2019-11-17 17:49:16 +0800

    remove publish script

--- publish.sh
+++
@@ -1,6 +0,0 @@
-  
-npm run site:build;
-cp ./404.html ./dist/qing-form-doc/404.html;
-git add dist --force;
-git commit -m "demo: build demo";
-git subtree push --prefix dist/qing-form-doc origin gh-pages;
